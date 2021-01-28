c4eec0ed6447070c515be7a340c854306dba3212
Eldar Giniyatov <ekb.from@gmail.com>
2021-01-28 18:12:19 +0500

    Fix vue.config & package.json

--- vue.config.js
+++ vue.config.js
@@ -1,41 +1,6 @@
 const sass = require('sass');
-// const CopyPlugin = require('copy-webpack-plugin');
-const ImageminPlugin = require('imagemin-webpack-plugin').default;
-const ImageMinMozJpeg = require('imagemin-mozjpeg');
 
 module.exports = {
-    devServer: {
-        proxy: `http://localhost:${process.env.APP_NODEMON_PORT}`,
-    },
-    configureWebpack: {
-        plugins: [
-            // new CopyPlugin({
-            //     patterns: [
-            //         {
-            //             context: './src/assets/',
-            //             from: 'img/',
-            //             to: 'img/',
-            //         },
-            //         {
-            //             from: './src/assets/img/new/favicon',
-            //             to: './',
-            //         },
-            //     ],
-            // }),
-            new ImageminPlugin({
-                disable: process.env.NODE_ENV !== 'production',
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                plugins: [
-                    ImageMinMozJpeg({
-                        quality: 85,
-                        progressive: true,
-                    }),
-                ],
-                cacheFolder: '/tmp/cache/img',
-            }),
-        ],
-    },
-
     chainWebpack: (config) => {
         config.module
             .rule('vue')
@@ -56,10 +21,5 @@ module.exports = {
             },
         },
         extract: false,
-    },
-
-    pluginOptions: {
-        lintStyleOnBuild: false,
-        stylelint: {},
     },
 };
